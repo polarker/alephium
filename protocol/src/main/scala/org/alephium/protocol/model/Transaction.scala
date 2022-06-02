3b6115c1f97907db851880d9a0740a00f1448012
Cheng Wang <wachmc@gmail.com>
2022-06-02 12:42:15 +0200

    wip

--- protocol/src/main/scala/org/alephium/protocol/model/Transaction.scala
+++ protocol/src/main/scala/org/alephium/protocol/model/Transaction.scala
@@ -54,6 +54,8 @@ sealed trait TransactionAbstract {
       AssetOutputRef.from(output, TxOutputRef.key(id, index))
     }
   }
+
+  def isEntryMethodPayable: Boolean = unsigned.scriptOpt.exists(_.entryMethod.isPayable)
 }
 
 final case class Transaction(
